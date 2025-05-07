7f3ae9c8450d0b1da01d031eb3ef538149e9bdc6
Tsafi tanos <hristos.ioanidis@gmail.com>
2025-05-08 01:03:30 +0200

    chore: edited the navbar and added the mockData in the Cards

--- src/components/cardGrid.jsx
+++ src/components/cardGrid.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Grid, Typography, Card, CardContent } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Grid,
+  Typography,
+  Card,
+  CardContent,
+  CircularProgress,
+} from '@mui/material';
+import mockData from '../mockData';
 
-const pokemonCard = () => {
+const getPokemonCard = (pokemonId) => {
+  const [pokemonData, setPokemonData] = useState(mockData);
+  console.log(pokemonData[`${pokemonId}`]);
   return (
-    <Grid item xs={12} sm={6} md={4}>
+    <Grid size={{ xs: 12, sm: 6, md: 4 }} key={pokemonId}>
       <Card className='card'>
         <CardContent>
           <Typography variant='h5' component='div'>
-            Pokemon Name
+            {`${pokemonData[`${pokemonId}`].name}`}
           </Typography>
           <Typography variant='body2' color='text.secondary'>
-            Pokemon Details
+            {`ID: ${pokemonId}`}
           </Typography>
         </CardContent>
       </Card>
@@ -19,57 +28,28 @@ const pokemonCard = () => {
 };
 
 const CardGrid = () => {
+  const [pokemonData, setPokemonData] = useState(mockData);
   return (
-    <Grid container spacing={2} className='card-grid'>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-      <Grid className='grid-item' item xs={12} sm={6} md={4}>
-        {pokemonCard()}
-      </Grid>
-    </Grid>
+    <>
+      {!pokemonData || pokemonData.length === 0 ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+          }}
+        >
+          <CircularProgress color='secondary' />
+        </div>
+      ) : (
+        <Grid container spacing={2} className='card-grid'>
+          {Object.keys(pokemonData).map((pokemonId) =>
+            getPokemonCard(pokemonId),
+          )}
+        </Grid>
+      )}
+    </>
   );
 };
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,99 +1,120 @@
-import React, { useState } from 'react';
+import * as React from 'react';
 import {
   AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
   Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Button,
+  MenuItem,
 } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import {
+  Menu as MenuIcon,
+  CatchingPokemonSharp as PokemonIcon,
+} from '@mui/icons-material';
+import '../index.css';
 
-const Navbar = () => {
-  const [mobileOpen, setMobileOpen] = useState(false);
+const pages = ['Home', 'Pokemons', 'About'];
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
   };
 
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center', width: 250 }}>
-      <Typography variant='h6' sx={{ my: 2 }}>
-        Pokemon App
-      </Typography>
-      <List>
-        <ListItem button>
-          <ListItemText primary='Home' />
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary='Pokemons' />
-        </ListItem>
-        <ListItem button>
-          <ListItemText primary='About' />
-        </ListItem>
-      </List>
-    </Box>
-  );
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   return (
-    <AppBar position='sticky' sx={{ backgroundColor: '#0099FF' }}>
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <IconButton
-          color='inherit'
-          aria-label='open drawer'
-          edge='start'
-          onClick={handleDrawerToggle}
-          sx={{ display: { sm: 'none' } }}
-        >
-          <MenuIcon />
-        </IconButton>
-
-        <Typography
-          variant='h6'
-          component='div'
-          sx={{
-            flexGrow: 1,
-            textAlign: { xs: 'right', sm: 'left' },
-          }}
-        >
-          Pokemon App
-        </Typography>
-
-        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-          <nav>
-            <ul
-              className='navbar-list'
-              style={{
-                display: 'flex',
-                gap: '16px',
-                listStyle: 'none',
-                margin: 0,
-                padding: 0,
+    <AppBar position='static'>
+      <Container maxWidth='xl'>
+        <Toolbar disableGutters>
+          <PokemonIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 5 }} />
+          <Typography
+            variant='h5'
+            noWrap
+            sx={{
+              display: { xs: 'none', md: 'flex' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            POKEMON APP
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton color='inherit' onClick={handleOpenNavMenu}>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
               }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
             >
-              <li className='navbar-item'>Home</li>
-              <li className='navbar-item'>Pokemons</li>
-              <li className='navbar-item'>About</li>
-            </ul>
-          </nav>
-        </Box>
-      </Toolbar>
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
 
-      <Drawer
-        anchor='left'
-        open={mobileOpen}
-        onClose={handleDrawerToggle}
-        ModalProps={{
-          keepMounted: true,
-        }}
-      >
-        {drawer}
-      </Drawer>
+          <PokemonIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 3 }} />
+          <Typography
+            variant='h5'
+            noWrap
+            sx={{
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            POKEMON APP
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: 'none', md: 'flex' },
+              justifyContent: 'flex-end',
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}></Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
-};
+}
 
 export default Navbar;
