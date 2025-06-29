529c0bbafb360b12bd05237e1a4d180e995d9265
Tsafi tanos <IHristos@users.noreply.github.com>
2025-06-29 16:43:11 +0200

    chore: added styling for the type icons

--- src/components/pokemonCard.jsx
+++ src/components/pokemonCard.jsx
@@ -2,16 +2,17 @@ import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 import '../css/cardGrid.css';
+import '../css/iconStyle.css';
 import { capitalizeFirstChar } from '../utils/capitalizeFirstChar';
-
 const PokemonCard = ({ pokemonId, pokemon }) => {
   const navigate = useNavigate();
   const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon?.id}.png`;
+  const typeIcons = import.meta.glob('../assets/typeIcons/*.svg', {
+    eager: true,
+  });
   const capitalizedName = capitalizeFirstChar(pokemon.name);
-
   console.log(pokemonId); // For debugging. Needs to be deleted when the project is finished.
   console.log(pokemon); // For debugging. Needs to be deleted when the project is finished.
-
   return (
     <Card
       className='card'
@@ -19,31 +20,66 @@ const PokemonCard = ({ pokemonId, pokemon }) => {
         navigate(`/pokemon/${pokemon.name}`, { state: { id: pokemonId } })
       }
     >
-      <CardMedia
-        className='pokemon-card-media'
-        image={sprite}
-        sx={{ width: 200, height: 200, mx: 'auto' }}
-      />
       <CardContent>
         <Typography variant='h5' component='div' sx={{ textAlign: 'center' }}>
           {capitalizedName ?? 'Unknown'}
         </Typography>
-        <Typography variant='h5' component='div' sx={{ textAlign: 'center' }}>
-          {(pokemon?.types || [])
-            .map((typeObj) => capitalizeFirstChar(typeObj?.type?.name))
-            .join(', ') || 'Unknown'}
+        <CardMedia
+          className='pokemon-card-media'
+          image={sprite}
+          sx={{ width: 200, height: 200, mx: 'auto' }}
+        />
+        <Typography
+          variant='h5'
+          component='div'
+          sx={{
+            textAlign: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexWrap: 'wrap',
+            gap: 1,
+          }}
+        >
+          {(pokemon?.types || []).map((typeObj) => {
+            const typeName = typeObj?.type?.name;
+            const iconKey = `../assets/typeIcons/${typeName}.svg`;
+            const iconSrc = typeIcons[iconKey]?.default;
+            return (
+              <span
+                key={typeName}
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  marginRight: 8,
+                }}
+              >
+                {iconSrc && (
+                  <img
+                    src={iconSrc}
+                    alt={typeName}
+                    className={`icon ${typeName}`}
+                    style={{ width: 24, height: 24, marginRight: 4 }}
+                  />
+                )}
+                {capitalizeFirstChar(typeName)}
+              </span>
+            );
+          })}
         </Typography>
-        <Typography variant='body2' color='text.secondary'>
+        <Typography
+          variant='body2'
+          color='text.secondary'
+          style={{ marginTop: '55px', textAlign: 'center' }}
+        >
           ID: {pokemon?.id ?? 'N/A'}
         </Typography>
       </CardContent>
     </Card>
   );
 };
-
 PokemonCard.propTypes = {
   pokemonId: PropTypes.number,
   pokemon: PropTypes.object,
 };
-
 export default PokemonCard;
